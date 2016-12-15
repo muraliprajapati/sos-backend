53576f9322dd9b76373bb28667229c82e48c975e
murali-prajapati <murali.prajapati555@gmail.com>
2016-12-16 00:16:10 +0530

    group edit api added

--- app/group.py
+++ app/group.py
@@ -22,6 +22,9 @@ class CreateGroup(Resource):
         group_photo = group_json['photo']
         group_admin = group_json['adminId']
         group_users_list = group_json['userlist']
+        member_id_list = group_users_list.split(',')
+        if not member_id_list: abort(400)
+
         admin = User.query.filter_by(id=group_admin).first()
         group = Group(name=group_name, photo=group_photo, adminId=group_admin,
                       adminName=admin.name, userlist=group_users_list)
@@ -29,8 +32,6 @@ class CreateGroup(Resource):
         db.session.flush()
         db.session.refresh(group)
         db.session.commit()
-        member_id_list = group_users_list.split(',')
-
 
         members = []
         for id in member_id_list:
@@ -51,5 +52,53 @@ class CreateGroup(Resource):
         }
 
 
+class EditGroup(Resource):
+    def put(self, id):
+        # send notification to new added and removed members
+        group = Group.query.filter_by(id=id).first()
+        group_json = request.get_json()
+        if not group_json: abort(400)
+        group.name = group_json['name']
+        group.photo = group_json['photo']
+        group.adminId = group_json['adminId']
+        group.userlist = group_json['userlist']
+        member_id_list = group_json['userlist'].split(',')
+        member_id_list = map(int,member_id_list)
+
+        if not member_id_list: abort(400)
+        db.session.commit()
+
+        old_members = group.g_users
+        removed_members = []
+        members = []
+        for u in old_members:
+            # print u.name
+            if u.id not in member_id_list:
+                print u.id
+                group.g_users.remove(u)
+                removed_members.append(u)
+        db.session.commit()
+        for u in removed_members:
+            print u.name
+
+        for uid in member_id_list:
+            print uid
+
+            user = User.query.filter_by(id=uid).first()
+            if user and user not in old_members:
+                group.g_users.append(user)
+            op = marshal(user, data)
+            members.append(op)
+        db.session.commit()
+        op1 = marshal(group, group_response)
+
+        return {
+            'code': 200,
+            'description': 'OK',
+            'group': op1,
+            'member': members
+        }
+
 
 api.add_resource(CreateGroup, '/group')
+api.add_resource(EditGroup, '/group/<int:id>')
